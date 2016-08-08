778a0074c59a31366db5df1380f7af92b2c1c291
Tommy <setacross@gmail.com>
2016-08-08 16:19:43 +0700

    user section working 100 percent - except security

--- app/Http/Controllers/AdminUsersController.php
+++ app/Http/Controllers/AdminUsersController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\UsersEditRequest;
 use App\Http\Requests\UsersRequest;
 use App\Photo;
 use App\Role;
@@ -50,7 +51,13 @@ class AdminUsersController extends Controller
      */
     public function store(UsersRequest $request)
     {
-        $input = $request->all();
+        if(trim($request->password) == ''){
+            $input = $request->except('password');
+        }else{
+
+            $input = $request->all();
+        }
+
 
         if($file = $request->file('photo_id')){
 
@@ -84,7 +91,10 @@ class AdminUsersController extends Controller
      */
     public function edit($id)
     {
-        return view('admin.users.edit',compact('id'));
+        $user = User::findOrFail($id);
+
+        $roles = Role::lists('name','id')->all();
+        return view('admin.users.edit',compact('user','roles'));
     }
 
     /**
@@ -94,9 +104,34 @@ class AdminUsersController extends Controller
      * @param  int  $id
      * @return \Illuminate\Http\Response
      */
-    public function update(Request $request, $id)
+    public function update(UsersEditRequest $request, $id)
     {
-       //
+        $user= User::findOrFail($id);
+
+        if(trim($request->password) == ''){
+            $input = $request->except('password');
+        }else{
+
+            $input = $request->all();
+        }
+
+        if($file = $request->file('photo_id')){
+
+            $name = time().$file->getClientOriginalName();
+
+            $file->move('images',$name);
+
+            $photo = Photo::create(['file'=>$name]);
+
+            $input['photo_id'] = $photo->id;
+        }
+
+        $input['password'] = bcrypt($request->password);
+
+        $user->update($input);
+
+        return redirect('/admin/users');
+
     }
 
     /**
---
+++ resources/views/admin/users/edit.blade.php
@@ -0,0 +1,52 @@
+@extends('layouts.admin')
+
+@section('content')
+
+    <h1>Edit User</h1>
+    <div class="row">
+        <div class="col-sm-3">
+            <img class="img-responsive img-rounded"
+                 src="{{$user->photo ? $user->photo->file : 'http://placehold.it/400x400'}}" alt="">
+        </div>
+        <div class="col-sm-9">
+
+
+
+            {!! Form::model($user,['method'=>'PATCH',
+                'action'=>['AdminUsersController@update',$user->id],'files'=>true]) !!}
+            <div class="form-group">
+                {!! Form::label('name','Name:') !!}
+                {!! Form::text('name',null,['class'=>'form-control']) !!}
+            </div>
+            <div class="form-group">
+                {!! Form::label('email','Email:') !!}
+                {!! Form::text('email',null,['class'=>'form-control']) !!}
+            </div>
+
+            <div class="form-group">
+                {!! Form::label('role_id','Role:') !!}
+                {!! Form::select('role_id',['' => 'Chose option']+ $roles,null,['class'=>'form-control']) !!}
+            </div>
+            <div class="form-group">
+                {!! Form::label('is_active','Status:') !!}
+                {!! Form::select('is_active',array(1 => 'Active',0 =>'Not Active'),null,['class'=>'form-control']) !!}
+            </div>
+            <div class="form-group">
+                {!! Form::label('photo_id','Photo:') !!}
+                {!! Form::file('photo_id',null,['class'=>'form-control']) !!}
+            </div>
+            <div class="form-group">
+                {!! Form::label('password','Password') !!}
+                {!! Form::password('password',['class'=>'form-control']) !!}
+            </div>
+            <div class="form-group">
+                {!! Form::submit('Update User',['class'=>'btn btn-primary form-control']) !!}
+            </div>
+
+            {!! Form::close() !!}
+        </div>
+    </div>
+    <div class="row">
+    @include('errors.error')
+    </div>
+@endsection
